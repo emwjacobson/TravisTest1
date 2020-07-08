f3b0649d9094197577bdf3e8d8f423bb5b6e02c8
Emerson Jacobson <emwjacobson@gmail.com>
2020-07-08 11:19:01 -0700

    Made a program to square numbers.

--- square.cpp
+++ square.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -9,13 +11,13 @@ int main(int argc, char* argv[]) {
     }
 
     string a(argv[1]);
-    int num = 0;
+    float num = 0;
     try {
-        num = stoi(a);
+        num = stof(a);
     } catch (const invalid_argument &ia) {
         cout << "Error, number not entered. Usage: " << argv[0] << " #" << endl;
         return 1;
     }
-    cout << num << endl;
+    cout << pow(num, 2) << endl;
     return 0;
 }
